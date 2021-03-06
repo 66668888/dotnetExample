3ca90f1038f0bdf5fa4ce8d2ad42acddba1d24e6
myProject <1046646472@qq.com>
2021-03-06 11:27:20 +0800

    XmlSerializer
    [major]:Add XmlSerializer Demo.

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExampleProject.XmlSerializerHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,12 @@ using System.Threading.Tasks;
 
 namespace ExampleProject
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            SelectChoose a = new SelectChoose();
+            var ac = a.Serializer<OptionArray>(a.SelectOption());
         }
     }
 }
---
+++ ExampleProject/XmlSerializerHelper/SelectChoose.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ExampleProject.XmlSerializerHelper
+{
+    public class SelectChoose
+    {
+        public string SelectOption()
+        {
+            return
+                @"<OptionArray><ItemOption><Name>name1</Name><Value>value1</Value><Index>1</Index></ItemOption><ItemOption><Name>name2</Name><Value>value2</Value><Index>2</Index></ItemOption></OptionArray>";
+
+        }
+
+        public T Serializer<T>(string option) where T:class
+        {
+            using (StringReader stringReader = new StringReader(option))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                return xmlSerializer.Deserialize(stringReader) as T;
+            }
+        }
+    }
+
+    [Serializable]
+    public class OptionArray
+    {               
+        public List<ItemOption> ItemOption { get; set; }
+    }
+
+    [Serializable]
+    public class ItemOption
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
