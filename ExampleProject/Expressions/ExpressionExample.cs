8b8cf89121033605a2bf4bd637a4c393efcf9ddd
myProject <1046646472@qq.com>
2021-03-06 09:22:10 +0800

    Expression
    [major]:Added Expression demo.

---
+++ ExampleProject/Expressions/ExpressionExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExampleProject.Expressions
+{
+    public class ExpressionExample
+    {
+        public  void ExpressionDemo()
+        {
+            var param = Expression.Parameter(typeof(Shopper), "o");
+            var prop = Expression.Property(param, nameof(Shopper.ShopperId));
+            var constant = Expression.Constant(10134);
+            var exp = Expression.Equal(param, constant);
+
+            prop = Expression.Property(param, nameof(Shopper.ShopperName));
+            constant = Expression.Constant("zhenqi");
+            var filter = Expression.Call(prop,
+                typeof(string).GetMethod("Contains", new Type[] { typeof(string) }), constant);
+            exp = Expression.Add(exp, filter);
+
+            var expression = Expression.Lambda(exp, param) as Expression<Func<Shopper, bool>>;
+            
+        }
+        
+    }
+
+    public class Shopper
+    {
+        public int ShopperId { get; set; }
+        public string ShopperName { get; set; }
+    }
+}
